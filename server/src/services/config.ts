4ac5afe6aea460eec86559dfbd7e93e5ea685896
Alexey T <rostovboy@gmail.com>
2025-09-26 11:39:38 +0300

    Added config service to get and save data

--- server/src/services/config.ts
+++ server/src/services/config.ts
@@ -1,11 +1,51 @@
 import type { Core } from '@strapi/strapi';
 
+interface ConfigData {
+  selectedExportCollections: string[];
+  selectedImportCollections: string[];
+}
+
 const configService = ({ strapi }: { strapi: Core.Strapi }) => ({
-  getConfig() {
-    return 'Config is here';
+  async getConfig(): Promise<ConfigData> {
+    const configs = await strapi.entityService.findMany(
+      'plugin::strapi-xlsx-impexp-plugin.xlsx-impexp-plugin-config',
+      { filters: {} }
+    );
+
+    if (configs && configs.length > 0) {
+      return configs[0] as ConfigData;
+    }
+
+    return {
+      selectedExportCollections: [],
+      selectedImportCollections: [],
+    };
   },
-  saveConfig() {
-    return 'Config Saved';
+
+  async saveConfig(data: ConfigData) {
+    const { selectedExportCollections, selectedImportCollections } = data;
+
+    if (!Array.isArray(selectedExportCollections) || !Array.isArray(selectedImportCollections)) {
+      throw new Error('selectedExportCollections and selectedImportCollections must be arrays');
+    }
+
+    const existingConfigs = await strapi.entityService.findMany(
+      'plugin::strapi-xlsx-impexp-plugin.xlsx-impexp-plugin-config',
+      { filters: {} }
+    );
+
+    if (existingConfigs.length > 0) {
+      return strapi.entityService.update(
+        'plugin::strapi-xlsx-impexp-plugin.xlsx-impexp-plugin-config',
+        existingConfigs[0].id,
+        { data: { selectedExportCollections, selectedImportCollections } as any }
+      );
+    }
+
+    return strapi.entityService.create(
+      'plugin::strapi-xlsx-impexp-plugin.xlsx-impexp-plugin-config',
+      { data: { selectedExportCollections, selectedImportCollections } as any }
+    );
   }
 });
 
